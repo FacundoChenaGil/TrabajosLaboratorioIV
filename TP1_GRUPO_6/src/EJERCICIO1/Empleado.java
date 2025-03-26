3702ee307951bb18411b5849c7ce16e5addc4b9c
victoriaareyes <mavireyes2012@gmail.com>
2025-03-26 11:34:03 -0300

    Update Empleado.java
    Se añaden atributos y se emplea el concepto de encapsulación en la clase Empleado.

--- TP1_GRUPO_6/src/EJERCICIO1/Empleado.java
+++ TP1_GRUPO_6/src/EJERCICIO1/Empleado.java
@@ -1,5 +1,29 @@
 package EJERCICIO1;
 
 public class Empleado {
-
+	//atributos
+	private int id;
+	private String nombre;
+	private int edad;
+	
+	
+	//getters y setters
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 }
