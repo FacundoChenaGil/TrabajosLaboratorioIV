c1df387dedf3f262897fbef2dfc32293e8f3719a
JustiLeon <juusleon@gmail.com>
2025-03-31 17:57:41 -0300

    punto 7

--- TP1_GRUPO_6/src/EJERCICIO1/Principal.java
+++ TP1_GRUPO_6/src/EJERCICIO1/Principal.java
@@ -5,13 +5,17 @@ public class Principal {
 	public static void main(String[] args) {
 		Empleado[] vEmpleados = new Empleado[5];
 		
+		String[] nombres = {"Justina", "Victoria", "Gabriela", "Facundo", "Ernesto"};
+		int[] edades = {22, 21, 20, 19, 18};
+		
+		
 		// Creacion de Empleados
 		for (int i=0;i<5;i++) {
 			if (i==2) {
 				vEmpleados[i] = new Empleado();
 			}
 			else {
-				vEmpleados[i] = new Empleado("Empleado " + (i + 1), i + 20);
+				vEmpleados[i] = new Empleado(nombres[i], edades[i]); // Constructor con parámetros
 			}
 			
 			Empleado emp = vEmpleados[i];
@@ -19,6 +23,10 @@ public class Principal {
 			System.out.println(emp.toString());
 			System.out.println("El próximo ID será el " + Empleado.devuelveProximoID());
 		}
+		
+		
 	}
+	
+	
 
 }
