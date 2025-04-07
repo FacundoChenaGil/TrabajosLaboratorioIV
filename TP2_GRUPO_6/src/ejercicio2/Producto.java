9faaf18c23fffb45650b22ccd136c4759169e9bb
victoriaareyes <mavireyes2012@gmail.com>
2025-04-07 16:53:19 -0300

    EJ 2. Se crea el paquete "ejercicio2" y se crea la clase Producto con sus atributos, getters y setters.

---
+++ TP2_GRUPO_6/src/ejercicio2/Producto.java
@@ -0,0 +1,22 @@
+package ejercicio2;
+
+public class Producto {
+	//atributos
+	private String FechaCaducidad;
+	private String NroLote;
+	
+	//getters and setters
+	public String getFechaCaducidad() {
+		return FechaCaducidad;
+	}
+	public void setFechaCaducidad(String fechaCaducidad) {
+		FechaCaducidad = fechaCaducidad;
+	}
+	public String getNroLote() {
+		return NroLote;
+	}
+	public void setNroLote(String nroLote) {
+		NroLote = nroLote;
+	}
+
+}
