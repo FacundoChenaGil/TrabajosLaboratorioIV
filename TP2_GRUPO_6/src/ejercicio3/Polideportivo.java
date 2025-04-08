1dc1e58d197947279962ffc7fe90d26bffde87e9
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2025-04-08 00:20:41 -0300

    Cargo las clases del ejercicio 3 y hago el implement de las interfaces en Polideportivo

---
+++ TP2_GRUPO_6/src/ejercicio3/Polideportivo.java
@@ -0,0 +1,47 @@
+package ejercicio3;
+
+public class Polideportivo implements IEdificio, IInstalacionDeportiva  {
+	private String name;
+	private double superficie;
+	private int tipoDeInstalacion;
+	
+	public Polideportivo() {
+		this.name = "sin nombre";
+		this.superficie = 0;
+		this.tipoDeInstalacion = 0;
+	}
+	
+	public Polideportivo(String name, double superficie, int tipoDeInstalacion) {
+		this.name = name;
+		this.superficie = superficie;
+		this.tipoDeInstalacion = tipoDeInstalacion;
+	}
+
+	public void setSuperficieEdificio(double superficieEdificio) {
+		this.superficie = superficieEdificio;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setTipoDeInstalacion(int tipoDeInstalacion) {
+		this.tipoDeInstalacion = tipoDeInstalacion;
+	}
+
+	@Override
+	public int getTipoDeInstalacion() {
+		return this.tipoDeInstalacion;
+	}
+
+	@Override
+	public double getSuperficieEdificio() {
+		// TODO Auto-generated method stub
+		return this.superficie;
+	}
+
+}
---
+++ TP2_GRUPO_6/src/ejercicio3/mainEjercicio3.java
@@ -0,0 +1,9 @@
+package ejercicio3;
+
+public class mainEjercicio3 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
