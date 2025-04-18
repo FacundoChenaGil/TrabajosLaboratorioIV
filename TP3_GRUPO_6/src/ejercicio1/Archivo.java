bf29ae06b9b36763d81c5203803b6029ccc8f1d6
PuppyProgrammer <facuchena@hotmail.com>
2025-04-17 23:57:20 -0300

    se crea metodo Guardar en Archivo

--- TP3_GRUPO_6/src/ejercicio1/Archivo.java
+++ TP3_GRUPO_6/src/ejercicio1/Archivo.java
@@ -1,9 +1,14 @@
 package ejercicio1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeSet;
 
 public class Archivo {
 	
+	private static final String Ruta = "C:\\TrabajosLaboratorioIV\\TP3_GRUPO_6\\Archivos\\Personas.txt";
 	
 	 public static boolean archivoExiste(String rutaArchivo) {
 	        File archivo = new File(rutaArchivo);
@@ -13,18 +18,47 @@ public class Archivo {
 	        return false;
 	    }
 	 
-	
-	public static void verificarDniInvalido (String dni) throws DniInvalido{
-		Boolean hayCaracteres = false;
-		for(int i=0; i<dni.length(); i++) {
-			if(!Character.isDigit(dni.charAt(i))) {
-				hayCaracteres = true;
-			}
-		}
-		
-		if(hayCaracteres) {
-			throw new DniInvalido();
-		}
-	}
-	
+	 public static TreeSet<Persona> Guardar() {
+		 TreeSet<Persona> ts = new TreeSet<Persona>();
+		 FileReader entrada;
+		 try {
+			 
+			 entrada = new FileReader(Ruta);
+			 BufferedReader miBuffer = new BufferedReader(entrada);
+			 String linea = "";
+			 
+			 while((linea = miBuffer.readLine()) != null) {
+				 String[] bloques = linea.split("-");
+				 
+				 if(bloques.length == 3) {
+					 String nombre = bloques[0];
+					 String apellido = bloques[1];
+					 String dni = bloques[2];
+					 
+					 try {
+						 Persona.VerificarDniInvalido(dni);
+						 Persona persona = new Persona();
+						 
+						 persona.setNombre(nombre);
+						 persona.setApellido(apellido);
+						 persona.setDni(dni);
+						 
+						 ts.add(persona);
+						 
+					 }catch(DniInvalido e) {
+						 System.out.println(e.getMessage());
+					 }
+				 }
+				 
+				 
+			 } 
+			 miBuffer.close();
+		 }catch(IOException e) {
+			 System.out.println("Error al leer el Archivo");
+			 e.printStackTrace();
+		 }
+		 
+		 return ts;
+	 }
 }
+
--- TP3_GRUPO_6/src/ejercicio1/Principal.java
+++ TP3_GRUPO_6/src/ejercicio1/Principal.java
@@ -1,5 +1,8 @@
 package ejercicio1;
 
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class Principal {
 
 	public static void main(String[] args) {
@@ -9,8 +12,14 @@ public class Principal {
 	        } else {
 	            System.out.println(" El archivo no existe.");
 	        }
+		 TreeSet<Persona> ts = Archivo.Guardar();
+		 
+		 Iterator<Persona> it = ts.iterator();
+		 while (it.hasNext()) {
+			 Persona persona = it.next();
+			 System.out.println(persona.toString());
+		 }
 	    }
-
 	}
 
 
