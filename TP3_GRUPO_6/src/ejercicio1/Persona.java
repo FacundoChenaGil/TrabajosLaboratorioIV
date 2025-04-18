7e96204ef2ab2ab802eb5aed482fecb699aea7cd
PuppyProgrammer <facuchena@hotmail.com>
2025-04-17 22:27:29 -0300

    Se define la clase Persona

--- TP3_GRUPO_6/src/ejercicio1/Persona.java
+++ TP3_GRUPO_6/src/ejercicio1/Persona.java
@@ -1,5 +1,51 @@
 package ejercicio1;
 
 public class Persona {
+	private String Nombre;
+	private String Apellido;
+	private String Dni;
+	
+	public Persona() {
+		
+	}
+	
+	public Persona(String nombre, String apellido, String dni) {
+		this.Nombre = nombre;
+		this.Apellido = apellido;
+		this.Dni = dni;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellido() {
+		return Apellido;
+	}
+
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+
+	public String getDni() {
+		return Dni;
+	}
+
+	public void setDni(String dni) {
+		Dni = dni;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [Nombre=" + Nombre + ", Apellido=" + Apellido + ", Dni=" + Dni + "]";
+	}
+	
+	
+	
+	
 
 }
