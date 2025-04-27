a5f5786489cb68b1b37b31baca621a94719dfd66
victoriaareyes <mavireyes2012@gmail.com>
2025-04-27 15:35:37 -0300

    Se evalúa que se haya seleccionado un sistema operativo.

--- TP4_GRUPO_6/src/Ejercicio_3/EventoBtnAceptar.java
+++ TP4_GRUPO_6/src/Ejercicio_3/EventoBtnAceptar.java
@@ -3,6 +3,7 @@ package Ejercicio_3;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 
 public class EventoBtnAceptar implements ActionListener {
@@ -18,7 +19,22 @@ public class EventoBtnAceptar implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		//SISTEMA OPERATIVO
+		String so = "";
+				
+		if(rdbtnWindows.isSelected()) {
+			so = "Windows";
+		}
+		else if(rdbtnLinux.isSelected()) {
+			so = "Linux";
+		}
+		else if(rdbtnMac.isSelected()) {
+			so = "Mac";
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Debe seleccionar un sistema operativo.", "Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
 		
 	}
 
