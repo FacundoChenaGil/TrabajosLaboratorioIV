b5263d1256573cab7233f1a34be7ee758b226045
victoriaareyes <mavireyes2012@gmail.com>
2025-05-19 21:48:07 -0300

    Se agrega el driver al Build Path.

--- TP6_GRUPO_6/src/main/Principal.java
+++ TP6_GRUPO_6/src/main/Principal.java
@@ -9,6 +9,6 @@ public class Principal {
 	public static void main(String[] args) {
 		PersonaDao dao = new PersonaDaoImpl();
 		
-		dao.insert(new Persona("385533293", "Ernesto", "Riveiro"));
+		dao.insert(new Persona("11111111", "Justina", "Leon"));
 	}
 }
