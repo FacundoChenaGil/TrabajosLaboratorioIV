e68fd7647d5f68a19bd4469a381cac178280a5d1
Ernesto José Riveiro <ernesto.ernzxr@gmail.com>
2025-05-27 23:27:11 -0300

    Se crean clases funcionales para poder listar los Seguros
    • Se crea la clase Seguro.
    • Se crea la clase SeguroDao.
    • Se crea servletSeguro.
    • Se crean los JSP del ejercicio.
    • Se crea el metodo obtenerSeguros que recibe una opcion para filtrar los seguros.

---
+++ TP7_GRUPO_6/src/main/java/dominio/SeguroDao.java
@@ -0,0 +1,75 @@
+package dominio;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class SeguroDao {
+	private String host = "jdbc:mysql://localhost:3306/";
+	private String user = "root";
+	private String pass = "root";
+	private String dbName = "segurosgroup?useSSL=false";
+	
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Seguro> obtenerSeguros(String filtroTipo) {	
+		ArrayList<Seguro> lista = new ArrayList<Seguro>();
+		Connection conn = null;
+		PreparedStatement pSt = null;
+		ResultSet rs = null;
+		
+		try{
+			conn = DriverManager.getConnection(host + dbName, user, pass);
+			
+			String query = "SELECT idSeguro,s.descripcion,s.idTipo,ts.descripcion AS descripcionTipo,costoContratacion,costoAsegurado " +
+						   "FROM seguros AS s " +
+						   "INNER JOIN tiposeguros AS ts ON s.idTipo = ts.idTipo";
+			
+			if(filtroTipo != null && !filtroTipo.equals("0")) {
+				query += " WHERE s.idTipo = ?";
+				pSt = conn.prepareStatement(query);
+				pSt.setInt(1, Integer.parseInt(filtroTipo));
+			}
+			else {
+				pSt = conn.prepareStatement(query);
+			}
+
+			
+			rs = pSt.executeQuery();
+			
+			while(rs.next()){
+				
+				Seguro seguroRs = new Seguro();
+				seguroRs.setId(rs.getInt("idSeguro"));
+				seguroRs.setDescripcion(rs.getString("descripcion"));
+				seguroRs.setIdTipo(rs.getInt("idTipo"));
+				seguroRs.setDescripcionTipo(rs.getString("descripcionTipo"));
+				seguroRs.setCostoContratacion(rs.getFloat("costoContratacion"));
+				seguroRs.setCostoAsegurado(rs.getFloat("costoAsegurado"));
+				
+				lista.add(seguroRs);
+			}
+
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			try {
+				conn.close();
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return lista;
+	}
+}
